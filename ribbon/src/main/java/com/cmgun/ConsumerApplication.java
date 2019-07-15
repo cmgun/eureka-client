1f5b2b1cfd67269d8a615ae1e2365acb65511597
cmgun <cm199311@qq.com>
2019-07-15 23:59:41 +0800

    ribbon consumer

---
+++ ribbon/src/main/java/com/cmgun/ConsumerApplication.java
@@ -0,0 +1,29 @@
+package com.cmgun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * consumer with ribbon
+ *
+ * Created by cmgun on 2019/7/15
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerApplication {
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ ribbon/src/main/java/com/cmgun/controller/ConsumerController.java
@@ -0,0 +1,25 @@
+package com.cmgun.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * consumer controller
+ *
+ * Created by cmgun on 2019/7/15
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+}
